db2865225a1039f6833810c712d1b59b6257d17f
Deepak <Deepak.k13@outlook.com>
2025-04-01 14:56:59 +0530

    feat: add router configuration to app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,29 @@
-console.log('Hello');
+import Notes from './components/Notes.js';
+import Note from './components/Note.js';
+import Archive from './components/Archives.js';
+import Router from './service/router.js';
+
+function init() {
+  const app = document.querySelector('.app');
+
+  if (!app) throw new Error('Application mount point not found');
+
+  const routes = [
+    {
+      path: '/',
+      component: new Notes(app)
+    },
+    {
+      path: '/archives',
+      component: new Archive(app)
+    },
+    {
+      path: '/note/:title',
+      component: new Note(app)
+    }
+  ];
+
+  Router.init(routes);
+}
+
+window.addEventListener('DOMContentLoaded', init);
